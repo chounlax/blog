03cdc2604613fb1bfa7ec0acb445f452203981ef
Zlatan <sd.thompson80200@gmail.com>
2026-02-06 11:10:14 +0100

    Ajout du transfert de donnée

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -5,6 +5,11 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use App\Form\ArticleType;
+use Symfony\Component\HttpFoundation\Request;
+use App\Entity\Article;
+use Doctrine\ORM\EntityManagerInterface;
+
 
 final class BlogController extends AbstractController
 {
@@ -17,10 +22,23 @@ final class BlogController extends AbstractController
     }
 
     #[Route('/article', name: 'app_article')]
-    public function article(): Response
+    public function article(Request $request, EntityManagerInterface $em): Response
     {
+        $article = new Article();
+        $form = $this->createForm(ArticleType::class, $article);
+        if ($request->isMethod('POST')){
+            $form->handleRequest($request);
+            if ($form->isSubmitted()&&$form->isValid()){
+                $em->persist($article);
+                 $em->flush();
+
+                $this->addFlash('notice','Article envoyé');
+                return $this->redirectToRoute('app_article');
+            }
+            }
         return $this->render('blog/article.html.twig', [
-            
+            'form' => $form->createView()
+
         ]);
     }
 }
