4b9d363065207443ff22020eec6535e4f8fbe83e
raphaël <coursierap@icloud.com>
2026-02-06 10:28:21 +0100

    Création de la table Categorie dans la bd ; préparation du controller

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -5,14 +5,22 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use App\Form\CategorieType;
 
 final class BlogController extends AbstractController
 {
     #[Route('/', name: 'app_blog')]
     public function index(): Response
     {
+        $form = $this->createForm(CategorieType::class);
         return $this->render('blog/index.html.twig', [
-            
+            'form' => $form->createView()  
         ]);
     }
+    #[Route('/categorie', name: 'app_categorie')]
+public function categorie(): Response
+{
+return $this->render('blog/categorie.html.twig', [
+]);
+}
 }
