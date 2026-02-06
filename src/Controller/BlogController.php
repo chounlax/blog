3cc8e9f35e5278b7e030686aeba29d83d52f900e
raphaël <coursierap@icloud.com>
2026-02-06 09:47:51 +0100

    Ajout de la page d'acceuil, bootstrap et préconfiguration de la page Catégorie

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -8,11 +8,11 @@ use Symfony\Component\Routing\Attribute\Route;
 
 final class BlogController extends AbstractController
 {
-    #[Route('/blog', name: 'app_blog')]
+    #[Route('/', name: 'app_blog')]
     public function index(): Response
     {
         return $this->render('blog/index.html.twig', [
-            'controller_name' => 'BlogController',
+            
         ]);
     }
 }
