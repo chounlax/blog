30e0a88252cb398f2a3db60eda243fbb3a55ebf3
raphaël <coursierap@icloud.com>
2026-02-06 10:53:09 +0100

    Finalisation 1.0 de la page complète : Catégorie

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -6,6 +6,8 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use App\Form\CategorieType;
+use App\Entity\Categorie;
+use Doctrine\ORM\EntityManagerInterface;
 
 final class BlogController extends AbstractController
 {
@@ -18,9 +20,23 @@ final class BlogController extends AbstractController
         ]);
     }
     #[Route('/categorie', name: 'app_categorie')]
-public function categorie(): Response
+public function categorie(Request $request, EntityManagerInterface $em): Response
 {
-return $this->render('blog/categorie.html.twig', [
+    $categorie = new Contact();
+    $form = $this->createForm(CategorieType::class, $categorie);
+		
+    if($request->isMethod('POST')){
+        $form->handleRequest($request);
+        if ($form->isSubmitted()&&$form->isValid()){
+            $em->persist($categorie);	
+				$em->flush();
+            $this->addFlash('notice','Message envoyé'); 
+            return $this->redirectToRoute('app_categorie');
+        }
+    }
+    
+    return $this->render('blog/categorie.html.twig', [
+        'form' => $form->createView()
 ]);
 }
 }
