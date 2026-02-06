9da6dee46ceb1d1db1f0b6c7cd8603b38341e13f
raphaël <coursierap@icloud.com>
2026-02-06 10:38:32 +0100

    Mise en forme de la page Catégorie et avancement du controlleur Catégorie

--- src/Form/CategorieType.php
+++ src/Form/CategorieType.php
@@ -6,13 +6,17 @@ use App\Entity\Categorie;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 class CategorieType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('nom')
+            ->add('nom', TextType::class, ['attr' => ['class'=> 'form-control'], 'label_attr' => ['class'=>'fw-bold']])
+            ->add('envoyer', SubmitType::class, ['attr' => ['class'=> 'btn bg-primary text-white m-4' ],'row_attr' => ['class' => 'text-center'],])
+
         ;
     }
 
